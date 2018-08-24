e2ea3c9368125511f2ba6f8fe1ca4c764056c610
Student <example@example.com>
2018-08-24 13:28:44 -0700

    Add cards for size, type, toppings, pizza preview

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
-var activeScreen = "intro"
+var sections = ["size","type","toppings",]
+var activeSection = "intro";
 $(function(){
-  onFromRight('button#start-button')
+  onFromRight('button#start-button');
   $('#start-button').click(function(){
     off('button#start-button');
     onFromRight('button#previous-button');
     onFromRight('button#next-button');
-    $('.section#size-card').css({
-      'transform': 'scaleY(1)'
+    activeSection = sections[0]
+    $('.section#'+activeSection+'-card').slideDown(600)
+    $('.section').css({
+      'opacity': '1'
     })
+    $('#pizza-card').css({
+      'transform': 'translateX(0%)',
+      'opacity': '1'
+    })
+  });
+  $('#next-button').click(function(){
+    $('.section#'+activeSection+'-card').slideUp()
+    activeSection = sections[sections.indexOf(activeSection)+1]
+    $('.section#'+activeSection+'-card').slideDown()
   })
-})
+  $('#previous-button').click(function(){
+    if (sections.indexOf(activeSection) > 0) {
+      $('.section#'+activeSection+'-card').slideUp()
+      activeSection = sections[sections.indexOf(activeSection)-1]
+      $('.section#'+activeSection+'-card').slideDown()
+    }
+  });
+});
 function onFromRight(element) {
   $(element).css({
     'pointer-events': 'all',
-  })
+  });
   $(element).css({
     'left': '0vw',
     'opacity': '1',
@@ -22,5 +41,5 @@ function onFromRight(element) {
 function off(element) {
   $(element).css({
     'opacity': '0',
-  })
+  });
 }
